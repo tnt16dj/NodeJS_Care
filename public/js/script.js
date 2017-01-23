a670ce6a4816d80c825b07755d289730c53902dc
Todd Tkach <toddtkach@gmail.com>
2017-01-23 16:42:10 -0500

    Updating HTML insert to occur after API call

--- public/js/script.js
+++ public/js/script.js
@@ -3,34 +3,6 @@ $(window).load(function(){
     $('#jsonAnswer').empty();
 
     getCoverage(); 
-
-    // do something 
-    var data = $.parseJSON($("#jsonAnswer").html());
-    var coverage = new Coverage(data);
-
-    buildCoverageHTML = function(coverage) {
-    $(".coverage-section").remove();
-
-    var plugin = new CoveragePlugin(coverage);
-
-    // Adds the demographic section
-    plugin.addEligibleMetadataSection();
-    plugin.addDemographicsSection();
-    plugin.addInsuranceSection1();
-    plugin.addInsuranceSection2();
-    plugin.addInsuranceSection3();
-    plugin.addPlanMaximumMinimumDeductibles();
-    plugin.addPlanCoinsurance();
-    plugin.addPlanCopayment();
-    plugin.addPlanDisclaimer();
-    plugin.addAdditionalInsurancePolicies();
-    plugin.addGenericServices();
-
-
-    $('#eligibleContent').append(plugin.coverageSection);
-    };
-
-    buildCoverageHTML(coverage);
 });
 
 function getCoverage() {
@@ -43,6 +15,35 @@ function getCoverage() {
     request.done(function(msg) {
         alert(msg)
         $('#jsonAnswer').append(msg);
+
+        // do something 
+        var data = $.parseJSON($("#jsonAnswer").html());
+        var coverage = new Coverage(data);
+
+        buildCoverageHTML = function(coverage) {
+        $(".coverage-section").remove();
+
+        var plugin = new CoveragePlugin(coverage);
+
+        // Adds the demographic section
+        plugin.addEligibleMetadataSection();
+        plugin.addDemographicsSection();
+        plugin.addInsuranceSection1();
+        plugin.addInsuranceSection2();
+        plugin.addInsuranceSection3();
+        plugin.addPlanMaximumMinimumDeductibles();
+        plugin.addPlanCoinsurance();
+        plugin.addPlanCopayment();
+        plugin.addPlanDisclaimer();
+        plugin.addAdditionalInsurancePolicies();
+        plugin.addGenericServices();
+
+
+        $('#eligibleContent').append(plugin.coverageSection);
+        };
+
+        buildCoverageHTML(coverage);
+        
     });
         request.fail(function(jqXHR, textStatus) {
         alert( "Request failed: " + textStatus );
