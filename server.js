29eba7d637fa9b9a1ea9536989045cd864ce5b67
myershm <hmm1513@gmail.com>
2017-01-24 22:09:57 +0000

    Update server.js
    Harry Updates

--- server.js
+++ server.js
@@ -65,3 +65,87 @@ var port = process.env.PORT || 3000;
 app.listen(port,function(){
   console.log("Live at Port " + port);
 });
+
+app.post("/signup",urlencodedParser,function(req,res){
+	var fname = req.body.fname;
+	var lname= req.body.lname;
+	var emailaddr = req.body.emailaddr;
+	var passwrd = req.body.passwrd;
+	
+	var masterfile = {};
+	masterfile.members = [];
+	
+//Read from current File
+
+	var obj = jsonfile.readFileSync(file)
+	retObj = JSON.parse(obj);
+	//console.log(obj)	
+	
+	var x = 0;
+	for (i = 0, ilen = retObj.members.length; i<ilen; i++){
+		
+		if (retObj.members[i].emailaddr == emailaddr) {
+			x = 1;
+		}
+		
+	}
+//		console.log("AFTER CHECK")
+		
+		if(x == 0) {
+			//console.log("NOT equal")
+			var memberfile = {}; //NEW Object
+			memberfile.fname = fname;
+			memberfile.lname = lname;
+			memberfile.emailaddr = emailaddr;
+			memberfile.passwrd = passwrd;
+			memberfile.providername = req.body.providername;
+			memberfile.memberid = req.body.memberid;
+			
+			retObj.members.push(memberfile);
+	
+			var jsonString = JSON.stringify(retObj);
+	
+			jsonfile.writeFile(file,jsonString)
+			
+			res.sendFile(path + "index.html");
+		}
+		else {
+			res.sendFile(path + "login.html");
+		}
+  
+	
+}
+);
+
+app.post("/login",urlencodedParser,function(req,res){
+	var emailaddr = req.body.email;
+	var passwrd = req.body.password;
+	
+	var obj = jsonfile.readFileSync(file)
+	retObj = JSON.parse(obj);
+	
+	var x = 0;
+	for (i = 0, ilen = retObj.members.length; i<ilen; i++){
+		
+		//console.log("From the File " + retObj.members[i].emailaddr)
+		//console.log("ScrennPW= " + emailaddr)
+		
+		if (retObj.members[i].emailaddr == emailaddr) {
+			if (retObj.members[i].passwrd == passwrd) {
+				x = 1;
+			}
+		}
+		
+	}
+	
+	if(x == 0) {
+		// user not found - send back to login
+		res.sendFile(path + "login.html");
+	}
+	else {
+		// user was found - send back to login
+		res.sendFile(path + "index.html");
+	}
+  
+  res.sendFile(path + "index.html");
+});
